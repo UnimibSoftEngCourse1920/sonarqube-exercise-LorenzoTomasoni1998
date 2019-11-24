91aa7b25f68581e44a1e198d40c526b036fab3a9
Lorenzo Tomasoni <l.tomasoni4@campus.unimib.it>
2019-11-24 18:59:16 +0100

    secondo issue (di tipo brain-overload):
    Nella classe Timeout nel package org.junit.rules, alla riga 41 (e in
    tutte le parti del codice che facevano riferimento all'attributo
    timeout) ho
    modificato l'attributo che si chiamava timeout in timer. Questo perchè
    spesso porta confusione avere
    un attributo che si chiama come la classe di cui fa parte.

--- src/main/java/org/junit/rules/Timeout.java
+++ src/main/java/org/junit/rules/Timeout.java
@@ -7,7 +7,7 @@ import org.junit.runners.model.Statement;
 import java.util.concurrent.TimeUnit;
 
 /**
- * The Timeout Rule applies the same timeout to all test methods in a class:
+ * The Timeout Rule applies the same timer to all test methods in a class:
  * <pre>
  * public static class HasGlobalLongTimeout {
  *
@@ -26,19 +26,19 @@ import java.util.concurrent.TimeUnit;
  * }
  * </pre>
  * <p>
- * Each test is run in a new thread. If the specified timeout elapses before
+ * Each test is run in a new thread. If the specified timer elapses before
  * the test completes, its execution is interrupted via {@link Thread#interrupt()}.
  * This happens in interruptable I/O and locks, and methods in {@link Object}
  * and {@link Thread} throwing {@link InterruptedException}.
  * <p>
- * A specified timeout of 0 will be interpreted as not set, however tests will
- * still launch from separate threads. This can be useful for disabling timeouts
+ * A specified timer of 0 will be interpreted as not set, however tests will
+ * still launch from separate threads. This can be useful for disabling timers
  * in environments where they are dynamically set based on some property.
  *
  * @since 4.7
  */
 public class Timeout implements TestRule {
-    private final long timeout;
+    private final long timer;
     private final TimeUnit timeUnit;
     private final boolean lookForStuckThread;
 
@@ -52,7 +52,7 @@ public class Timeout implements TestRule {
     }
 
     /**
-     * Create a {@code Timeout} instance with the timeout specified
+     * Create a {@code Timeout} instance with the timer specified
      * in milliseconds.
      * <p>
      * This constructor is deprecated.
@@ -61,7 +61,7 @@ public class Timeout implements TestRule {
      * {@link Timeout#millis(long)}, or {@link Timeout#seconds(long)}.
      *
      * @param millis the maximum time in milliseconds to allow the
-     * test to run before it should timeout
+     * test to run before it should timer
      */
     @Deprecated
     public Timeout(int millis) {
@@ -69,16 +69,16 @@ public class Timeout implements TestRule {
     }
 
     /**
-     * Create a {@code Timeout} instance with the timeout specified
+     * Create a {@code Timeout} instance with the timer specified
      * at the timeUnit of granularity of the provided {@code TimeUnit}.
      *
-     * @param timeout the maximum time to allow the test to run
-     * before it should timeout
-     * @param timeUnit the time unit for the {@code timeout}
+     * @param timer the maximum time to allow the test to run
+     * before it should timer
+     * @param timeUnit the time unit for the {@code timer}
      * @since 4.12
      */
-    public Timeout(long timeout, TimeUnit timeUnit) {
-        this.timeout = timeout;
+    public Timeout(long timer, TimeUnit timeUnit) {
+        this.timer = timer;
         this.timeUnit = timeUnit;
         lookForStuckThread = false;
     }
@@ -90,13 +90,13 @@ public class Timeout implements TestRule {
      * @since 4.12
      */
     protected Timeout(Builder builder) {
-        timeout = builder.getTimeout();
+        timer = builder.getTimeout();
         timeUnit = builder.getTimeUnit();
         lookForStuckThread = builder.getLookingForStuckThread();
     }
 
     /**
-     * Creates a {@link Timeout} that will timeout a test after the
+     * Creates a {@link Timeout} that will timer a test after the
      * given duration, in milliseconds.
      *
      * @since 4.12
@@ -106,7 +106,7 @@ public class Timeout implements TestRule {
     }
 
     /**
-     * Creates a {@link Timeout} that will timeout a test after the
+     * Creates a {@link Timeout} that will timer a test after the
      * given duration, in seconds.
      *
      * @since 4.12
@@ -116,12 +116,12 @@ public class Timeout implements TestRule {
     }
 
     /**
-     * Gets the timeout configured for this rule, in the given units.
+     * Gets the timer configured for this rule, in the given units.
      *
      * @since 4.12
      */
     protected final long getTimeout(TimeUnit unit) {
-        return unit.convert(timeout, timeUnit);
+        return unit.convert(timer, timeUnit);
     }
 
     /**
@@ -136,7 +136,7 @@ public class Timeout implements TestRule {
 
     /**
      * Creates a {@link Statement} that will run the given
-     * {@code statement}, and timeout the operation based
+     * {@code statement}, and timer the operation based
      * on the values configured in this rule. Subclasses
      * can override this method for different behavior.
      *
@@ -145,7 +145,7 @@ public class Timeout implements TestRule {
     protected Statement createFailOnTimeoutStatement(
             Statement statement) throws Exception {
         return FailOnTimeout.builder()
-            .withTimeout(timeout, timeUnit)
+            .withTimeout(timer, timeUnit)
             .withLookingForStuckThread(lookForStuckThread)
             .build(statement);
     }
@@ -169,7 +169,7 @@ public class Timeout implements TestRule {
      */
     public static class Builder {
         private boolean lookForStuckThread = false;
-        private long timeout = 0;
+        private long timer = 0;
         private TimeUnit timeUnit = TimeUnit.SECONDS;
 
         protected Builder() {
@@ -179,25 +179,25 @@ public class Timeout implements TestRule {
          * Specifies the time to wait before timing out the test.
          *
          * <p>If this is not called, or is called with a
-         * {@code timeout} of {@code 0}, the returned {@code Timeout}
+         * {@code timer} of {@code 0}, the returned {@code Timeout}
          * rule instance will cause the tests to wait forever to
          * complete, however the tests will still launch from a
-         * separate thread. This can be useful for disabling timeouts
+         * separate thread. This can be useful for disabling timers
          * in environments where they are dynamically set based on
          * some property.
          *
-         * @param timeout the maximum time to wait
-         * @param unit the time unit of the {@code timeout} argument
+         * @param timer the maximum time to wait
+         * @param unit the time unit of the {@code timer} argument
          * @return {@code this} for method chaining.
          */
-        public Builder withTimeout(long timeout, TimeUnit unit) {
-            this.timeout = timeout;
+        public Builder withTimeout(long timer, TimeUnit unit) {
+            this.timer = timer;
             this.timeUnit = unit;
             return this;
         }
 
         protected long getTimeout() {
-            return timeout;
+            return timer;
         }
 
         protected TimeUnit getTimeUnit()  {
@@ -205,7 +205,7 @@ public class Timeout implements TestRule {
         }
 
         /**
-         * Specifies whether to look for a stuck thread.  If a timeout occurs and this
+         * Specifies whether to look for a stuck thread.  If a timer occurs and this
          * feature is enabled, the rule will look for a thread that appears to be stuck
          * and dump its backtrace.  This feature is experimental.  Behavior may change
          * after the 4.12 release in response to feedback.
